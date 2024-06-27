28da68bd41db2f36683b885afc2852a1b66f2d6e
Rahul Singh Nagesh <98824774+Hexton09@users.noreply.github.com>
2024-06-27 18:27:24 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Front-End, Back-end I'm the guy"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
