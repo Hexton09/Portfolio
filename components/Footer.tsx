a21aace3995b8e0635ae85eed0f2f0a6cffff2dc
Rahul Singh Nagesh <98824774+Hexton09@users.noreply.github.com>
2024-06-28 20:28:34 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,8 +17,7 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+          Like my work <span className="text-purple">Hire</span> Me
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
